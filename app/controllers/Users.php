e8cc4dfded282e0c42a698bec3094a246f16dbbc
welidlabidi <56021644+welidlabidi@users.noreply.github.com>
2021-02-12 17:14:44 +0100

    first

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -13,6 +13,48 @@ class Users extends Controller
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Process form
 
+            // sanitize post data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            //init data
+            $data = [
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => ''
+
+            ];
+            // Validation
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter email';
+            }
+            if (empty($data['name'])) {
+                $data['name_err'] = 'Please enter name';
+            }
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter password';
+            } elseif (strlen($data['password']) < 6) {
+                $data['password_err'] = 'Password must be at least 6 characters';
+            }
+            if (empty($data['confirm_password'])) {
+                $data['confirm_password_err'] = 'Please confirm password';
+            } else {
+                if ($data['password'] != $data["confirm_password"]) {
+                    $data['confirm_password_err'] = 'Password do not match';
+                }
+            }
+
+            // Make sure errors are empty
+            if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
+                //Validated
+                die('SUCCES');
+            } else {
+                $this->view('users/register', $data);
+            }
         } else {
             //Init data
             $data = [
@@ -28,6 +70,56 @@ class Users extends Controller
             ];
             // load view
             $this->view('users/register', $data);
+        }
+    }
+
+    public function login()
+    {
+        //check for post
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Process form
+
+            // sanitize post data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            //init data
+            $data = [
+
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'email_err' => '',
+                'password_err' => ''
+            ];
+
+            // Validation
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter email';
+            }
+
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter password';
+            }
+
+            // Make sure errors are empty
+            if (empty($data['email_err'])  && empty($data['password_err'])) {
+                //Validated
+                die('SUCCES');
+            } else {
+                $this->view('users/login', $data);
+            }
+        } else {
+            //Init data
+            $data = [
+                'email' => '',
+                'password' => '',
+                'email_err' => '',
+                'password_err' => '',
+
+            ];
+
+
+            // load view
+            $this->view('users/login', $data);
         }
     }
 }
--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -5,7 +5,6 @@
             aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
@@ -20,7 +19,7 @@
                     <a class="nav-link" aria-current="page" href="<?php echo URLROOT; ?>/users/register">Register</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT; ?>/views/users/login">Login</a>
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
                 </li>
             </ul>
         </div>
